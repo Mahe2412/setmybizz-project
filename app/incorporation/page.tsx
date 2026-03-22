4e74940478083f7f95457bb00e83238afd0fec78
Mahe2412 <kmahendra.kolli@gmail.com>
2026-03-22 15:45:57 +0530

    refactor: delete all redundant legacy components and directories

--- app/incorporation/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import IncorporationDashboard from "@/components/incorporation/IncorporationDashboard";
-
-export default function IncorporationPage() {
-    return <IncorporationDashboard />;
-}
